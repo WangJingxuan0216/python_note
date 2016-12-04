8fa893cac0faa4ce6756c2b6f06b01237bdc02e9
WangJingxuan0216 <wang.jingxuan0216@gmail.com>
2016-12-04 16:35:09 +0800

    a collection of progress bar in python

---
+++ progress_bar.py
@@ -0,0 +1,22 @@
+# A progress bar
+
+## Sample 1
+# from __future__ import division
+# import sys,time
+
+# j='#'
+if __name__ == '__main__':
+	for i in range(1,61):
+# 		j += '#'
+# 		sys.stdout.write(str(int((i/60)*100))+'% ||'+j+'->'+'\r')
+# 		sys.stdout.flush()
+# 		time.sleep(0.5)
+
+##Sample 2
+# from __future__ import division
+# import sys,time
+# if __name__ == '__main__':
+# 	for i in range(1,61):
+		sys.stdout.write('#'+'->'+'\b\b')
+		sys.stdout.flush()
+		time.sleep(0.5)
